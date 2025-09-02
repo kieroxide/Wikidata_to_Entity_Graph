f178aabf055faafee0adf93283fd636e4dbc9c8e
Kieroxide <kieranbprivate:gmail.com>
2025-09-02 12:49:25 +0100

    simplified main

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-from WikiGraph_Manager import WikiGraph_Manager
+from py.WikiGraph_Manager import WikiGraph_Manager
+import requests
+from urllib.parse import urlparse, unquote
 
 def main():
-    QIDS = [
-    "Q247",         # Half-Life 2
-    "Q1299",        # The Beatles
-    "Q42",          # Douglas Adams
-    "Q30",          # United States
-    "Q8338",        # Harry Potter
-    "Q140960",      # Marvel Cinematic Universe
-    "Q380",         # Star Wars
-    "Q317521",      # Elon Musk
-    "Q328",         # Wikipedia
-    "Q2065",        # Linux
-    "Q155",         # Pokémon
-    "Q17365",       # Minecraft
-    "Q76",          # Barack Obama
-    "Q95",          # Google
-    "Q93105",       # Iron Man
-    "Q84",          # London
-    "Q12010",       # Shrek
-    "Q134268",      # Breaking Bad
-    "Q134231",      # Pokémon Go
-    "Q29598584",    # OpenAI
-    "Q42",          # Douglas Adams (duplicate for testing)
-    "Q42",          
-]
     manager = WikiGraph_Manager()
-    manager.build(QIDS[0], 2)
+    manager.build("Q1", 15)
     manager.save_all()
+    manager.test_clean_results()
+    manager.test_results(console=True)
 
-main()
+if __name__ == "__main__":
+    main()
