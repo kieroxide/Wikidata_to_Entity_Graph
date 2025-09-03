d3f9ecf5095b7253fbb4a0894d8955fc116ab55a
Kieroxide <kieranbprivate:gmail.com>
2025-09-03 18:10:51 +0100

    Refactor WikiGraph_Manager to include relation_limit in build method

--- py/WikiGraph_Manager.py
+++ py/WikiGraph_Manager.py
@@ -1,6 +1,6 @@
-from py.Entity_Crawler import Entity_Crawler
-from py.Data_Handler import Data_Handler
-from py.Tester import Tester
+from .Entity_Crawler import Entity_Crawler
+from .Data_Handler import Data_Handler
+from .Tester import Tester
 
 class WikiGraph_Manager:
     """ Manages the process of building a Wikidata entity graph. """
@@ -10,22 +10,22 @@ class WikiGraph_Manager:
         self.entity_crawler = Entity_Crawler(self.data_handler)
         self.tester = Tester()
 
-    def build(self, source_id, depth=1, raw=False):
+    def build(self, source_id, depth=1, relation_limit = 5 ,raw=False):
         """ Crawls Wikidata starting from `source_id` up to `depth` levels
             and collects entity, property, and relation data."""
-        self.entity_crawler.crawl_wiki(source_id, crawl_depth=depth)
+        self.entity_crawler.crawl_wiki(source_id, crawl_depth=depth, relation_limit=relation_limit)
         
-        self.data_handler.fetch_relations_data(
+        return self.data_handler.fetch_relations_data(
             self.entity_crawler.entity_ids, 
             self.entity_crawler.property_ids, 
             self.entity_crawler.relations)
-    
-    def save_all(self, json_path=""):
+        
+    def save_all(self):
         """ Saves all gathered data to json files"""
-        self.data_handler.save_all(json_path)
+        self.data_handler.save_all()
     
-    def test_clean_results(self):
-        self.tester.clean_data(self.data_handler)
+    def test_clean_results(self, entities, properties, relations):
+        self.tester.clean_data(entities, properties, relations)
 
     def test_results(self, console=False):
         self.tester.test_entity_labels(self.data_handler, console)
@@ -33,3 +33,6 @@ class WikiGraph_Manager:
 
     def change_json_dir(self, json_dir):
         self.data_handler.change_json_dir(json_dir)
+    
+    def ensure_json_files_exist(self):
+        self.data_handler.ensure_json_files_exist()
